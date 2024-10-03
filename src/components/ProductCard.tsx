581a62b6808b0552421b5aef11286d451ed32c22
Mattias <mattiasjohansson.vfx@gmail.com>
2024-10-03 11:47:18 +0200

    Add link to ProductDetailsPage in ProductCard.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { IProduct } from "../interfaces";
 
 interface IProductCardProps {
@@ -7,15 +8,19 @@ interface IProductCardProps {
 export function ProductCard({ product }: IProductCardProps) {
   return (
     <article className="product-card b-radius-4 box-shadow">
-      <figure className="product-card__figure">
-        <img
-          src={product.thumbnail}
-          alt={`${product.title} packshot image`}
-          className="product-card__img"
-        />
-      </figure>
+      <Link className="product-card__link" to={`/product/${product.id}`}>
+        <figure className="product-card__figure">
+          <img
+            src={product.thumbnail}
+            alt={`${product.title} packshot image`}
+            className="product-card__img"
+          />
+        </figure>
+      </Link>
       <div className="product-card__wrapper">
-        <h4 className="product-card__title">{product.title}</h4>
+        <Link className="product-card__link" to={`/product/${product.id}`}>
+          <h4 className="product-card__title">{product.title}</h4>
+        </Link>
         <p className="product-card__rating">Rating: {product.rating}</p>
         <p className="product-card__price">$ {product.price}</p>
         <p className="product-card__stock">Stock: {product.stock}</p>
