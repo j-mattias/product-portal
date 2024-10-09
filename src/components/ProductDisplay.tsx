0cf282afd140a5adb3a7be0686d7ac609ff8df8c
Mattias <mattiasjohansson.vfx@gmail.com>
2024-10-09 09:37:43 +0200

    Edit ProductDisplay and Carousel CSS.

--- src/components/ProductDisplay.tsx
+++ src/components/ProductDisplay.tsx
@@ -11,16 +11,17 @@ export function ProductDisplay({product} :IProductDisplayProps) {
 
   return (
     <article className={`product-display`}>
-      <Link className="product-display__link" to={`/product/${product.id}`}>
+      <Link className="product-display__link link-wrapper" to={`/product/${product.id}`}>
         <figure className="product-display__figure">
           <img src={product.images[0]} alt={`${product.title} packshot image`} />
         </figure>
       </Link>
       <div className="product-display__wrapper flex-col">
-        <h2 className="product-display__title">{product.title}</h2>
+        <Link className="product-display__link" to={`/product/${product.id}`}>
+          <h2 className="product-display__title">{product.title}</h2>
+        </Link>
         {product.brand && <h4 className="product-display__brand">{product.brand}</h4>}
         <StarRating rating={product.rating} numStars={5} />
-        <h2>Description</h2>
         <p className="product-display__description">{product.description}</p>
         <h3 className="product-display__price">$ {product.price}</h3>
         <Stock stock={product.stock} />
