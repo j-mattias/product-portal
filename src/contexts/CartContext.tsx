d2014cf2a63ffe8865c84aad94b09834b952e101
Mattias <mattiasjohansson.vfx@gmail.com>
2024-10-11 10:33:22 +0200

    Edit Cart CSS and update updateItem in CartContext to remove item when quantity is 0.

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useContext, useState } from "react";
-import { ICart, IProduct } from "../interfaces";
+import { ICart, ICartItem, IProduct } from "../interfaces";
 
 type TOperand = "+" | "-";
 type TAction = "add" | "remove" | "clear";
@@ -28,19 +28,19 @@ export function CartContextProvider({ children }: ICartProviderProps) {
   // Used to clear an item from the cart
   const clearItem = (product: IProduct) => {
     setCart((c) => {
-      const cartItem = c.items.find(i => i.item.id === product.id);
+      const cartItem = c.items.find((i) => i.item.id === product.id);
 
       // If no item was found return
       if (!cartItem) return c;
 
       // Get the new total and get a new array without the cartItem
-      const newTotal = c.total - (cartItem.item.price * cartItem.quantity);
+      const newTotal = c.total - cartItem.item.price * cartItem.quantity;
       const arr = cart.items.filter((i) => i.item.id !== product.id);
 
       return {
         ...c,
         items: arr,
-        total: newTotal
+        total: newTotal,
       };
     });
   };
@@ -48,27 +48,29 @@ export function CartContextProvider({ children }: ICartProviderProps) {
   // Used to add or remove an item from the cart
   const updateItem = (product: IProduct, operand: TOperand, count = 1) => {
     setCart((c) => {
-
       // Find the product in the carts list of products
       const item = c.items.find((i) => i.item.id === product.id);
 
       // If item is already in the items list, else add the product to the cart
       if (item) {
-
-        console.log("setCart updating");
-        
         // Calculate the newQuantity, based on the operand and count
-        const newQauntity = operand === "+" ? item.quantity + count : Math.max(item.quantity - count, 0);
-        
+        const newQauntity =
+          operand === "+" ? item.quantity + count : Math.max(item.quantity - count, 0);
+
         // Get newTotal based on the total minus old item cost and adding new item cost based on newQuantity
         const price = product.price;
-        const newTotal = c.total - (price * item.quantity) + (price * newQauntity);
-        
-        // Get a new array with an updated cartItem at the specified index
-        const index = c.items.indexOf(item);
-        const arr = c.items.map((item, i) =>
-          i === index ? { ...item, quantity: newQauntity } : item
-        );
+        const newTotal = c.total - price * item.quantity + price * newQauntity;
+
+        let arr: ICartItem[];
+
+        // If the new quantity is 0 or less, remove it from the cart, else update cartItem
+        if (newQauntity <= 0) {
+          arr = c.items.filter((prevItem) => prevItem.item.id !== item.item.id);
+        } else {
+          // Get a new array with an updated cartItem at the specified index
+          const index = c.items.indexOf(item);
+          arr = c.items.map((item, i) => (i === index ? { ...item, quantity: newQauntity } : item));
+        }
 
         return {
           ...c,
@@ -76,16 +78,14 @@ export function CartContextProvider({ children }: ICartProviderProps) {
           total: newTotal,
         };
       } else {
-        console.log("setCart adding")
-
         // Add the product to the items list and update the price
         const arr = [...c.items, { item: product, quantity: count }];
-        const newTotal = c.total + (product.price * count);
+        const newTotal = c.total + product.price * count;
 
         return {
           ...c,
           items: arr,
-          total: newTotal
+          total: newTotal,
         };
       }
     });
@@ -99,7 +99,6 @@ export function CartContextProvider({ children }: ICartProviderProps) {
         break;
       }
 
-      // --- Removing until 0 will still keep the item in the cart ---
       case "remove": {
         updateItem(product, "-", count);
         break;
@@ -117,16 +116,18 @@ export function CartContextProvider({ children }: ICartProviderProps) {
 
   // Clears all the items from the cart
   const clearCart = () => {
-    setCart(c => {
+    setCart((c) => {
       return {
         ...c,
         items: [],
-        total: 0
-      }
-    })
+        total: 0,
+      };
+    });
   };
 
-  return <CartContext.Provider value={{ cart, updateCart, clearCart }}>{children}</CartContext.Provider>;
+  return (
+    <CartContext.Provider value={{ cart, updateCart, clearCart }}>{children}</CartContext.Provider>
+  );
 }
 
 export function useCartContext() {
@@ -138,5 +139,3 @@ export function useCartContext() {
 
   return context;
 }
-
-
