c325c686fef30bc0e3bf92674140f9d313a4feeb
Mattias <mattiasjohansson.vfx@gmail.com>
2024-10-18 11:29:24 +0200

    Update Alert CSS.

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -8,5 +8,5 @@ interface IAlertProps {
 export function Alert({ message, color }: IAlertProps) {
   const computedClass = color === "green" ? "alert-green" : "alert-red";
 
-  return <div className={`alert b-radius-4 ${computedClass}`}>{message}</div>;
+  return <div className={`alert b-radius-4 box-shadow ${computedClass}`}>{message}</div>;
 }
